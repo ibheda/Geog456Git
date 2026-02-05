af74ffe899297ceb74f7d37daf8bd945f61936a1
ibheda <ibheda@unc.edu>
2026-02-05 16:09:07 -0500

    Homework 3 commit

---
+++ HW3/Data/highpeaks.js
@@ -0,0 +1,195 @@
+var highpeaks = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-73.986536, 44.143664]
+      },
+      "properties": {
+        "FID": 0,
+        "FEATURE_ID": 973340,
+        "FEATURE_NA": "Algonquin Peak",
+        "FEATURE_CL": "Summit",
+        "STATE_ALPH": "NY",
+        "COUNTY_NAM": "Essex",
+        "ELEV_IN_M": 1559,
+        "ELEV_IN_FT": 5114,
+        "MAP_NAME": "North Elba",
+        "SHORT_NAME": "Algonquin"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-73.923476, 44.112554]
+      },
+      "properties": {
+        "FID": 1,
+        "FEATURE_ID": 973357,
+        "FEATURE_NA": "Mount Marcy",
+        "FEATURE_CL": "Summit",
+        "STATE_ALPH": "NY",
+        "COUNTY_NAM": "Essex",
+        "ELEV_IN_M": 1629,
+        "ELEV_IN_FT": 5344,
+        "MAP_NAME": "Mount Marcy",
+        "SHORT_NAME": "Mt Marcy"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-73.886253, 44.121164]
+      },
+      "properties": {
+        "FID": 2,
+        "FEATURE_ID": 973341,
+        "FEATURE_NA": "Basin Mountain",
+        "FEATURE_CL": "Summit",
+        "STATE_ALPH": "NY",
+        "COUNTY_NAM": "Essex",
+        "ELEV_IN_M": 1471,
+        "ELEV_IN_FT": 4827,
+        "MAP_NAME": "Mount Marcy",
+        "SHORT_NAME": "Basin Mtn"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-73.993758, 44.139775]
+      },
+      "properties": {
+        "FID": 3,
+        "FEATURE_ID": 973344,
+        "FEATURE_NA": "Boundary Peak",
+        "FEATURE_CL": "Summit",
+        "STATE_ALPH": "NY",
+        "COUNTY_NAM": "Essex",
+        "ELEV_IN_M": 1471,
+        "ELEV_IN_FT": 4826,
+        "MAP_NAME": "North Elba",
+        "SHORT_NAME": "Boundary Peak"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-73.786247, 44.082276]
+      },
+      "properties": {
+        "FID": 4,
+        "FEATURE_ID": 973347,
+        "FEATURE_NA": "Dix Mountain",
+        "FEATURE_CL": "Summit",
+        "STATE_ALPH": "NY",
+        "COUNTY_NAM": "Essex",
+        "ELEV_IN_M": 1480,
+        "ELEV_IN_FT": 4857,
+        "MAP_NAME": "Dix Mountain",
+        "SHORT_NAME": "Dix Mtn"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-73.934866, 44.111443]
+      },
+      "properties": {
+        "FID": 5,
+        "FEATURE_ID": 973351,
+        "FEATURE_NA": "Gray Peak",
+        "FEATURE_CL": "Summit",
+        "STATE_ALPH": "NY",
+        "COUNTY_NAM": "Essex",
+        "ELEV_IN_M": 1475,
+        "ELEV_IN_FT": 4840,
+        "MAP_NAME": "Mount Marcy",
+        "SHORT_NAME": "Gray Peak"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-73.90042, 44.105609]
+      },
+      "properties": {
+        "FID": 6,
+        "FEATURE_ID": 973353,
+        "FEATURE_NA": "Mount Haystack",
+        "FEATURE_CL": "Summit",
+        "STATE_ALPH": "NY",
+        "COUNTY_NAM": "Essex",
+        "ELEV_IN_M": 1512,
+        "ELEV_IN_FT": 4960,
+        "MAP_NAME": "Mount Marcy",
+        "SHORT_NAME": "Mt Haystack"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-73.998203, 44.136997]
+      },
+      "properties": {
+        "FID": 7,
+        "FEATURE_ID": 973355,
+        "FEATURE_NA": "Iroquois Peak",
+        "FEATURE_CL": "Summit",
+        "STATE_ALPH": "NY",
+        "COUNTY_NAM": "Essex",
+        "ELEV_IN_M": 1475,
+        "ELEV_IN_FT": 4840,
+        "MAP_NAME": "North Elba",
+        "SHORT_NAME": "Iroquois Peak"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-73.930699, 44.099499]
+      },
+      "properties": {
+        "FID": 8,
+        "FEATURE_ID": 973360,
+        "FEATURE_NA": "Mount Skylight",
+        "FEATURE_CL": "Summit",
+        "STATE_ALPH": "NY",
+        "COUNTY_NAM": "Essex",
+        "ELEV_IN_M": 1500,
+        "ELEV_IN_FT": 4920,
+        "MAP_NAME": "Mount Marcy",
+        "SHORT_NAME": "Mt Skylight"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-73.902646, 44.36588]
+      },
+      "properties": {
+        "FID": 9,
+        "FEATURE_ID": 973364,
+        "FEATURE_NA": "Whiteface Mountain",
+        "FEATURE_CL": "Summit",
+        "STATE_ALPH": "NY",
+        "COUNTY_NAM": "Essex",
+        "ELEV_IN_M": 1483,
+        "ELEV_IN_FT": 4867,
+        "MAP_NAME": "Lake Placid",
+        "SHORT_NAME": "Whiteface Mtn"
+      }
+    }
+  ]
+};
